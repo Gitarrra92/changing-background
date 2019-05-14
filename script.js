5833bd4552d2b92149a0a284bbbee233f11ddeba
Natalia <pilewska.natalia@gmail.com>
2019-05-14 14:12:04 +0200

    random hex added

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const myButton = document.querySelector(".colorBtn");
 const backgroundCl = document.querySelector("body");
+const hex = document.querySelector(".hex");
 
-let colors = ["yellow", "green", "blue", "#6845E1"];
+const hexNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A","B","C", "D", "E", "F"];
 
 myButton.addEventListener("click", changeBgr);
 
 function changeBgr() {
-    let randomColor = Math.floor(Math.random() * colors.length);
-    backgroundCl.style.backgroundColor = colors[randomColor];
+let hexCol = "#";
+
+for(let i = 0; i < 6; i++) {
+  let random = Math.floor(Math.random() * hexNumbers.length);
+  hexCol += hexNumbers[random];
+}
 
 }
+backgroundCl.style.backgroundColor = hexCol;
+hex.innerHTML = hexCol;
